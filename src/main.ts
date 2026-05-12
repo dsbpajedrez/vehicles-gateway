f14063d3aa690239358e4a64ec40243f1ed258cc
David <dsbpim@gmail.com>
2026-05-12 12:39:24 -0500

    feat: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import {
   createProxyMiddleware,
@@ -9,6 +10,34 @@ async function bootstrap() {
 
   const app =
     await NestFactory.create(AppModule);
+
+  // CONFIG SWAGGER
+  const config =
+    new DocumentBuilder()
+
+      .setTitle(
+        'Gateway API',
+      )
+
+      .setDescription(
+        'API Gateway de alquiler de vehiculos',
+      )
+
+      .setVersion('1.0')
+
+      .build();
+
+  const document =
+    SwaggerModule.createDocument(
+      app,
+      config,
+    );
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    document,
+  );
 
   // VEHICULOS
   app.use(
